eabc9de6faa313fdd1d8478e4ba429f55324f016
Jeremey <jeremey.duvall@automattic.com>
2016-02-21 07:37:48 -0700

    Add: Created todo component

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
 
 var Todo = React.createClass( {
-	return (
-		<div>A todo!</div>
-	)
+	render: function() {
+		return (
+			<div className="input-group">
+				<span className="input-group-addon">
+					<input type="checkbox" aria-label="..." />
+				</span>
+				<input type="text" className="form-control" value={ this.props.taskItem } disabled={ this.props.editing } />
+				<span className="input-group-btn">
+					<button className="btn btn-default" type="button">Edit</button>
+				</span>
+			</div>
+		);
+	}
 } );
 
 module.exports = Todo;
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var Todo = require('./todo');
 
 var taskDefault = "What do you have to do today?";
 
@@ -9,6 +10,16 @@ var TodoList = React.createClass( {
 	    };
 	},
 
+	componentDidMount: function() {
+		this.tasks = [];
+	},
+
+	resetAddField: function() {
+		this.setState( {
+			taskValue: taskDefault
+		} );
+	},
+
 	handleChange: function( e ) {
 		this.setState( {
 			taskValue: e.target.value
@@ -16,26 +27,43 @@ var TodoList = React.createClass( {
 	},
 
 	handleAdd: function () {
-		console.log(this.state.taskValue);
+		this.tasks.push(this.state.taskValue);
 
-		this.setState( {
-			taskValue: taskDefault
-		} );
+		this.resetAddField();
 	},
 
 	render: function() {
+		if ( this.tasks ) {
+			var taskList = this.tasks.map( function( task ) {
+				return (
+					<Todo
+						taskItem = {task}
+						key = { this.tasks.indexOf( task ) }
+						editing = { false }
+					/>
+				);
+			}.bind( this ) );
+		}
+
 		return (
 			<div>
 				<h2 className="todolist">Todo List</h2>
 				<div className="input-group">
   				<input type="text" className="form-control" placeholder={ this.state.taskValue } aria-describedby="basic-addon1" onChange={ this.handleChange }></input>
   				<span className="input-group-btn">
-        		<button className="btn btn-default" type="button" onClick={ this.handleAdd }>Add</button>
+        		<button className="btn btn-default" type="button" onClick={ this.handleAdd }>
+        			Add
+        		</button>
       		</span>
 				</div>
 				<div className="row">
-					<h4 className="completed col-md-6">Completed</h4>
-					<h4 className="incompleted col-md-6">Incompleted</h4>
+					<div className="completed col-md-6">
+						<h4>Completed</h4>
+					</div>
+					<div className="completed col-md-6">
+						<h4>Incompleted</h4>
+						{ taskList }
+					</div>
 				</div>
 			</div>
 		);
