10b5597dc8e72d3684c89a6cfa8f996b83975434
Jeremey <jeremey.duvall@automattic.com>
2016-03-06 20:04:06 -0700

    Fix: Completed some major work on todos - Checkbox now moves todo into the Completed column (not fully working) - Delete, Edit, and Save button all work on Incomplete side

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,15 +1,78 @@
 var React = require('react');
 
 var Todo = React.createClass( {
+	getInitialState: function() {
+		return {
+			editing: false,
+			deleted: false,
+			taskValue: "",
+			checked: null
+		};
+	},
+
+	componentWillMount: function() {
+		this.setState( {
+			taskValue: this.props.taskValue
+		} );
+	},
+
+	componentWillReceiveProps: function( nextProps ) {
+		if ( nextProps.taskValue !== this.state.taskValue ) {
+			this.setState( {
+				taskValue: nextProps.taskValue
+			} );
+		}
+	},
+
+	onEdit: function() {
+		this.setState( {
+			editing: true
+		} );
+	},
+
+	onSave: function() {
+		this.setState( {
+			editing: false
+		} );
+	},
+
+	handleDelete: function( item ) {
+		return function() {
+			this.setState( {
+				editing: false
+			} );
+
+			return this.props.deleteItem( item );
+		}.bind( this );
+	},
+
+	editTask: function( e ) {
+		this.setState( {
+			taskValue: e.target.value
+		} );
+	},
+
+	onCheck: function( item ) {
+		return function() {
+			return this.props.completedItem( item );
+		}.bind( this );
+
+		this.setState( {
+			checked: "checked"
+		} );
+	},
+
 	render: function() {
 		return (
-			<div className="input-group">
+			<div className="input-group" >
 				<span className="input-group-addon">
-					<input type="checkbox" aria-label="..." />
+					<input type="checkbox" aria-label="..." onClick={ this.onCheck( this ) } checked={ this.state.checked } />
 				</span>
-				<input type="text" className="form-control" value={ this.props.taskItem } disabled={ this.props.editing } />
+				<input type="text" className="form-control" value={ this.state.taskValue } disabled={ ! this.state.editing } onChange={ this.editTask } />
 				<span className="input-group-btn">
-					<button className="btn btn-default" type="button">Edit</button>
+					<button className={ "btn btn-default " + ( this.state.editing ? "hidden" : "" ) } type="button" onClick={ this.onEdit }>Edit</button>
+					<button className={ "btn btn-default " + ( this.state.editing ? "" : "hidden" ) } type="button" onClick={ this.handleDelete( this ) }>Delete</button>
+					<button className={ "btn btn-default " + ( this.state.editing ? "" : "hidden" ) } type="button" onClick={ this.onSave }>Save</button>
 				</span>
 			</div>
 		);
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,22 +1,23 @@
 var React = require('react');
 var Todo = require('./todo');
 
-var taskDefault = "What do you have to do today?";
-
 var TodoList = React.createClass( {
 	getInitialState: function() {
-	    return {
-	    	taskValue: taskDefault
-	    };
+		return {
+			taskValue: null,
+			tasks: [],
+			completedTasks: []
+		};
 	},
 
 	componentDidMount: function() {
 		this.tasks = [];
+		this.completedTasks = [];
 	},
 
 	resetAddField: function() {
 		this.setState( {
-			taskValue: taskDefault
+			taskValue: null
 		} );
 	},
 
@@ -27,38 +28,81 @@ var TodoList = React.createClass( {
 	},
 
 	handleAdd: function () {
-		this.tasks.push(this.state.taskValue);
+		if ( this.state.taskValue !== null ) {
+			this.tasks.push( this.state.taskValue );
+		} else {
+			alert( 'You have to type something' );
+		}
+
+		this.setState( {
+			tasks: this.tasks,
+		} );
 
 		this.resetAddField();
 	},
 
+	removeTask: function( task ) {
+		var index = this.state.tasks.indexOf( task.props.taskValue );
+
+		if ( index > -1 ) {
+			this.tasks.splice( index, 1 );
+		}
+	},
+
+	deleteItem: function( task ) {
+		this.removeTask( task );
+
+		this.setState( {
+			tasks: this.tasks
+		} );
+	},
+
+	completeItem: function( task ) {
+		this.removeTask( task );
+
+		this.completedTasks.push( task.props );
+
+		this.setState( {
+			tasks: this.tasks,
+			completedTasks: this.completedTasks
+		} );
+	},
+
+	buildTodo: function( task ) {
+		return (
+			<Todo
+				taskValue = { task }
+				key = { this.state.tasks.indexOf( task ) }
+				deleteItem = { this.deleteItem }
+				completedItem = { this.completeItem }
+			/>
+		);
+	},
+
 	render: function() {
 		if ( this.tasks ) {
-			var taskList = this.tasks.map( function( task ) {
-				return (
-					<Todo
-						taskItem = {task}
-						key = { this.tasks.indexOf( task ) }
-						editing = { false }
-					/>
-				);
-			}.bind( this ) );
+			var taskList = this.state.tasks.map( this.buildTodo );
+		}
+
+		if ( this.completedTasks ) {
+			var completedTasks = this.state.completedTasks.map( this.buildTodo );
 		}
 
 		return (
 			<div>
 				<h2 className="todolist">Todo List</h2>
 				<div className="input-group">
-  				<input type="text" className="form-control" placeholder={ this.state.taskValue } aria-describedby="basic-addon1" onChange={ this.handleChange }></input>
-  				<span className="input-group-btn">
-        		<button className="btn btn-default" type="button" onClick={ this.handleAdd }>
-        			Add
-        		</button>
-      		</span>
+  				<input type="text" className="form-control" value={ this.state.taskValue } aria-describedby="basic-addon1" onChange={ this.handleChange }></input>
+					<span className="input-group-btn">
+						<button className="btn btn-default" type="button" onClick={ this.handleAdd }>
+							Add
+						</button>
+					</span>
 				</div>
 				<div className="row">
 					<div className="completed col-md-6">
 						<h4>Completed</h4>
+						{ completedTasks }
 					</div>
 					<div className="completed col-md-6">
 						<h4>Incompleted</h4>
